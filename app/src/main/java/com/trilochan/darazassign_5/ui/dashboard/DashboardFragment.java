d10807748ab21662332028819ebef313324249af
Trilochankc <49303045+Trilochankc@users.noreply.github.com>
2020-01-07 20:49:26 +0545

    some code have been written and some modifications are needed

--- app/src/main/java/com/trilochan/darazassign_5/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/com/trilochan/darazassign_5/ui/dashboard/DashboardFragment.java
@@ -4,32 +4,85 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
-import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.trilochan.darazassign_5.ClassAdapter.AdapterFlash;
+import com.trilochan.darazassign_5.ClassAdapter.AdapterProduct;
+import com.trilochan.darazassign_5.ClassInterface.BaseAPI;
+import com.trilochan.darazassign_5.ClassInterface.RetrofitInterface;
+import com.trilochan.darazassign_5.ClassModel.FlashModalClass;
+import com.trilochan.darazassign_5.ClassModel.ProductCollectionModal;
 import com.trilochan.darazassign_5.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class DashboardFragment extends Fragment {
 
     private DashboardViewModel dashboardViewModel;
+    RecyclerView recyclerView_flash, recyclerView_product;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         dashboardViewModel =
                 ViewModelProviders.of(this).get(DashboardViewModel.class);
         View root = inflater.inflate(R.layout.fragment_dashboard, container, false);
-        final TextView textView = root.findViewById(R.id.text_dashboard);
-        dashboardViewModel.getText().observe(this, new Observer<String>() {
+
+        // rcyclerview code for flash sale
+        recyclerView_flash = root.findViewById(R.id.flashRecycler);
+        recyclerView_product = root.findViewById(R.id.productRecycler);
+        // Data listing through Arraylist
+        List<FlashModalClass> contactsList = new ArrayList<>();
+        contactsList.add(new FlashModalClass(R.drawable.sample_image_one_flash, "1,200", "12 Sold"));
+        contactsList.add(new FlashModalClass(R.drawable.sample_image_two_flash, "300", "18 Sold"));
+        contactsList.add(new FlashModalClass(R.drawable.sample_image_three_flash, "2,000", "20 Sold"));
+
+        // Flash Sale Set Adapter
+        AdapterFlash FlashAdapter = new AdapterFlash(getActivity(), contactsList);
+        LinearLayoutManager layoutManager
+                = new LinearLayoutManager(getActivity(), LinearLayoutManager.HORIZONTAL, false);
+        recyclerView_flash.setLayoutManager(layoutManager);
+        recyclerView_flash.setHasFixedSize(true);
+        recyclerView_flash.setAdapter(FlashAdapter);
+
+        // call function in order to load product
+        product_function();
+
+        return root;
+    }
+
+    public void product_function(){
+        RetrofitInterface retrofitApiInterface = BaseAPI.getRetrofit().create(RetrofitInterface.class);
+        Call<ProductCollectionModal> modalCollectionCall = retrofitApiInterface.parseProduct();
+        // modal bata hunna kei ni
+        modalCollectionCall.enqueue(new Callback<ProductCollectionModal>() {
             @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
+            public void onResponse(Call<ProductCollectionModal> call, Response<ProductCollectionModal> response) {
+                System.out.println("Collection list " + response.body());
+
+                AdapterProduct recyclerviewAdapter = new AdapterProduct(getActivity(),response.body().darazProductModalClasses());
+                RecyclerView.LayoutManager mlayoutManager = new GridLayoutManager(getContext(), 2);
+                recyclerView_product.setLayoutManager(mlayoutManager);
+                recyclerView_product.setHasFixedSize(true);
+                recyclerView_product.setAdapter(recyclerviewAdapter);
+                recyclerviewAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onFailure(Call<ProductCollectionModal> call, Throwable t) {
+
             }
         });
-        return root;
+
     }
 }
--- app/src/main/java/com/trilochan/darazassign_5/ui/notifications/NotificationsFragment.java
+++ app/src/main/java/com/trilochan/darazassign_5/ui/notifications/NotificationsFragment.java
@@ -6,13 +6,13 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
-import com.trilochan.darazassign_5.R;
+import com.simant.darazclone_simant.R;
 
 public class NotificationsFragment extends Fragment {
 
