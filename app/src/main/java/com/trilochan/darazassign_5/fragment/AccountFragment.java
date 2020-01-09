109cc552ab5a84cd431552af1c35771736d2de26
Trilochankc <49303045+Trilochankc@users.noreply.github.com>
2020-01-09 23:07:21 +0545

    fragments changes

--- app/src/main/java/com/trilochan/darazassign_5/fragment/AccountFragment.java
+++ app/src/main/java/com/trilochan/darazassign_5/fragment/AccountFragment.java
@@ -7,7 +7,6 @@ import android.preference.PreferenceManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
 import androidx.fragment.app.Fragment;
 import com.trilochan.darazassign_5.R;
 
--- app/src/main/java/com/trilochan/darazassign_5/fragment/HomeFragment.java
+++ app/src/main/java/com/trilochan/darazassign_5/fragment/HomeFragment.java
@@ -5,7 +5,6 @@ import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.GridLayoutManager;
