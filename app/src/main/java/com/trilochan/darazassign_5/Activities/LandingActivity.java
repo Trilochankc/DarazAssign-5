fc2888c15db50d6447879059d0d9255b5ef1e5f8
Trilochankc <49303045+Trilochankc@users.noreply.github.com>
2020-01-10 13:27:28 +0545

    modifications..

--- app/src/main/java/com/trilochan/darazassign_5/Activities/LandingActivity.java
+++ app/src/main/java/com/trilochan/darazassign_5/Activities/LandingActivity.java
@@ -18,10 +18,10 @@ public class LandingActivity extends AppCompatActivity {
         setContentView(R.layout.activity_landing);
 
         // In Activity's onCreate() for instance
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+     /*   if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
             Window w = getWindow();
-            w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+            w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);*/
         }
 
     }
-}
+
